29fbe237d94b78b4d5be9deb09d4141a22fef11c
Bao Ho <pippi.blockhead@gmail.com>
2021-03-14 00:16:44 +0700

    hello again euler

---
+++ project-euler/1.js
@@ -0,0 +1,30 @@
+// Multiples of 3 and 5
+
+// O(N)
+const solution = () => {
+  const N = 1000;
+  let res = 0;
+  for (let i = 1; i < N; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      res += i;
+    }
+  }
+  console.log(res);
+};
+
+// O(1)
+const solution2 = () => {
+  const N = 1000;
+
+  const sumDivisibleBy = (x) => {
+    const n = Math.floor((N - 1) / x);
+    return (x * (n * (n + 1))) / 2;
+  };
+
+  const res = sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15);
+
+  console.log(res);
+};
+
+solution();
+solution2();
